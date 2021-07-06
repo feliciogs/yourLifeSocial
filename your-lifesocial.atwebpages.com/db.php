5f6135398d5289aa17551633ed78113cac09c661
Felicio Gabriel <32279704+feliciogs@users.noreply.github.com>
2021-07-06 13:28:31 -0300

    Delete your-lifesocial.atwebpages.com directory

--- your-lifesocial.atwebpages.com/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-	error_reporting(E_ALL ^ E_DEPRECATED);
-	$connect = mysqli_connect("fdb21.awardspace.net","2759112_yourlifebd","Maxtarda@123") or die("Não Foi Possivel Conectar ao Servidor");
-	$db = mysqli_select_db($connect,"2759112_yourlifebd") or die("Impossível Conectar com a Base de Dados");
-
-	
-?>
